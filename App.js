a4fee6f29b89402cadb42636b9e08eb67e1aa955
bogere goldsoft <goldsoft25@gmail.com>
2019-09-15 20:31:50 +0300

    code cleanup

--- App.js
+++ App.js
@@ -7,7 +7,7 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+//import {Platform, StyleSheet, Text, View} from 'react-native';
 import {createStackNavigator} from 'react-navigation';
 import HomeScreen from './screens/home'
 import LoginScreen from './screens/login'
