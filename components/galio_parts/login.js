d998ef9946e483d586f60fdd49bdfdfb6f37d165
bogere goldsoft <goldsoft25@gmail.com>
2020-02-10 00:20:11 +0300

    removing styles in login

--- components/galio_parts/login.js
+++ components/galio_parts/login.js
@@ -82,21 +82,5 @@ class LoginScreen extends Component{
     }
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'space-around',
-        paddingTop: theme.SIZES.BASE * 0.3,
-        paddingHorizontal: theme.SIZES.BASE,
-        backgroundColor: theme.COLORS.WHITE,
-      },
-      social: {
-        width: theme.SIZES.BASE * 3.5,
-        height: theme.SIZES.BASE * 3.5,
-        borderRadius: theme.SIZES.BASE * 1.75,
-        justifyContent: 'center',
-      },
-  });
 
 export default LoginScreen 
